0a1f89b03c51a2d6d9dec75ee23ad039ae3947a4
alaasociumtech <alaa.abdalhadi@sociumtech.com>
2025-02-24 12:06:49 +0300

    updated functions

--- test_task.py
+++ test_task.py
@@ -8,37 +8,42 @@ from task import (
 )
 
 
-def test_get_post_by_id(mocker):
-    # test for success
+def test_get_post_by_id_success(mocker):
     mock_response = mocker.Mock()
     mock_response.json.return_value = {'id': 1}
     mocker.patch('task.http_get', return_value=mock_response)
     assert get_post_by_id(1) == {'id': 1}
-    # test for failure
+
+
+def test_get_post_by_id_failure(mocker):
     mocker.patch("task.http_get", side_effect=HTTPError("HTTP Error"))
     assert get_post_by_id(1) is None
 
 
-def test_get_posts_by_user_id(mocker):
-    # test for success
+def test_get_posts_by_user_id_success(mocker):
     mock_response = mocker.Mock()
     mock_response.json.return_value = {'id': 1}
     mocker.patch('task.http_get', return_value=mock_response)
     assert get_posts_by_user_id(1) == {'id': 1}
-    # test for failure
+
+
+def test_get_posts_by_user_id_failure(mocker):
     mocker.patch("task.http_get", side_effect=HTTPError("HTTP Error"))
     assert get_posts_by_user_id(1) is None
 
 
-def test_get_post_by_id_with_validation(mocker):
-    # test for success
+def test_get_post_by_id_with_validation_succsee(mocker):
     mock_response = mocker.Mock()
     mock_response.json.return_value = {'id': 1}
     mocker.patch('task.http_get', return_value=mock_response)
     assert get_post_by_id_with_validation(1) == {'id': 1}
-    # test for failure
+
+
+def test_get_post_by_id_with_validation_failure(mocker):
     mocker.patch("task.http_get", side_effect=HTTPError("HTTP Error"))
     assert get_post_by_id_with_validation(1) is None
-    # Test invalid post ID
+
+
+def test_get_post_by_id_with_validation_invalid_post_id(mocker):
     with pytest.raises(ValueError, match='post_id must be greater than 0'):
         get_post_by_id_with_validation(-1)
